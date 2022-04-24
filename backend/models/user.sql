2669af10ff0da859f9d107cb77e9075e3f4063ae
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-24 19:23:09 +0200

    fix db password, fullname et followers

--- backend/models/user.sql
+++ backend/models/user.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
 	username VARCHAR(50) NOT NULL PRIMARY KEY UNIQUE,
 	email VARCHAR(50) NOT NULL UNIQUE,
-	password VARCHAR(50) NOT NULL,
-	fullname VARCHAR(50) NOT NULL,
+	password VARCHAR(150) NOT NULL,
+	fullname VARCHAR(100) NOT NULL,
 	bio VARCHAR(50) NOT NULL,
 	following text[],
-	followers text[],
+	followers text[]
 );
